7e0a8bf9e5d9dab8fe39e0fb7f4aedc21c070a54
GabrieleLaMalfa <40953875+GabrieleLaMalfa@users.noreply.github.com>
2018-07-08 16:02:29 +0200

    Add files via upload

---
+++ rnn/RNN.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 30 09:06:22 2018
+
+@author: Gabriele
+"""
+import derivatives as derv
+import activations as actv
+
+
+import numpy as np
+
+################# RNN Forward and Backward Propagation #######################
+
+activations_dictionary = {"tanh": actv.tanh , "softmax": actv.softmax}
+derivatives_dictionary = {"dtanh": derv.dtanh , "dsoftmax": derv.dsoftmax}
+
+class RNN(object):
+    
+    def __init__(self , )
+
