a3d204cafc49c4f81ebbfc0d8942a21d46a7d2d2
Emanuele <emanuele.lamalfa@mail.polimi.it>
2018-12-23 16:22:25 +0100

    cnn-lstm experiments on topix with gaussian anomaly detection

---
+++ cnn-lstm/cnn_lstm_model.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 15:07:01 2018
+
+@author: Emanuele
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as scistats
+import tensorflow as tf
+
+import utils_topix as utils
+
+
+if __name__ == '__main__':
+    
+    # reset computational graph
+    tf.reset_default_graph()
+        
+    batch_size = 1
+    sequence_len = 10
+    learning_rate = 1e-3
+    
+    # define input/output pairs
+    input_ = tf.placeholder(tf.float32, [batch_size, sequence_len])
+    target = tf.placeholder(tf.float32, [batch_size, 1])
+    
+    # expand input to be supported by conv1d operation
+    input_ = tf.expand_dims(input_, -1)
+    
+    # define convolutional layer(s)
+    kernel_size = 3
+    number_of_channels = 1
+    number_of_filters = 25
+    
+    weights_conv = tf.Variable(tf.truncated_normal(shape=[kernel_size, 
+                                                          number_of_channels,
+                                                          number_of_filters]))
+    bias_conv = tf.Variable(tf.zeros(shape=[number_of_filters]))
+    
+    layer_conv = tf.nn.conv1d(input_, filters=weights_conv, stride=1, padding='SAME')
+    
+    layer_conv = tf.nn.relu(layer_conv)
+    
+    # flatten the output
+    dims = layer_conv.get_shape()
+    number_of_elements = dims[2:].num_elements()
+    layer_conv_flatten = tf.reshape(layer_conv, [batch_size, sequence_len, number_of_elements])
+    
+    # define lstm layer(s)
+    lstm_size = 128
+    number_of_lstm_layers = 1
+    
+    cell_lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+    layer_lstm = tf.contrib.rnn.MultiRNNCell([cell_lstm for _ in range(number_of_lstm_layers)])
+    init_state = layer_lstm.zero_state(batch_size, tf.float32)
+    outputs, states = tf.nn.dynamic_rnn(layer_lstm, layer_conv_flatten, initial_state=init_state)
+    
+    # dense layer extraction
+    output_lstm = outputs[:, -1, :]
+    weights_dense = tf.Variable(tf.truncated_normal(shape=[lstm_size, 1]))
+    bias_dense = tf.Variable(tf.zeros(shape=[1]))
+    
+    layer_dense = tf.matmul(output_lstm, weights_dense) + bias_dense
+    prediction = layer_dense  # linear activation
+    
+    # loss evaluation
+    loss = tf.losses.mean_squared_error(labels=target, predictions=prediction)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)        
+    
+    # extract train and test
+    x_train, y_train, x_valid, y_valid, x_test, y_test = utils.generate_batches(
+                                                             filename='data/Topix_index.csv', 
+                                                             window=sequence_len, mode='validation', 
+                                                             non_train_percentage=.3,
+                                                             val_rel_percentage=.6,
+                                                             normalize=True)
+    
+    # train the model
+    epochs = 25
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        
+        sess.run(init)
+        
+        # train
+        for e in range(epochs):
+            
+            print("epoch", e+1)
+            
+            iter_ = 0
+            
+            while iter_ < int(np.floor(x_train.shape[0] / batch_size)):
+        
+                batch_x = x_train[iter_*batch_size: (iter_+1)*batch_size, :, np.newaxis]
+                batch_y = y_train[iter_*batch_size: (iter_+1)*batch_size, np.newaxis]
+                
+                sess.run(optimizer, feed_dict={input_: batch_x,
+                                               target: batch_y})
+    
+                iter_ +=  1
+
+        # validation
+        errors_valid = np.zeros(shape=len(x_valid))
+        iter_ = 0
+        
+        while iter_ < int(np.floor(x_valid.shape[0] / batch_size)):
+    
+            batch_x = x_valid[iter_*batch_size: (iter_+1)*batch_size, :, np.newaxis]
+            batch_y = y_valid[iter_*batch_size: (iter_+1)*batch_size, np.newaxis]
+                
+            errors_valid[iter_] = sess.run(prediction-batch_y, feed_dict={input_: batch_x,
+                                                                 target: batch_y})
+
+            iter_ +=  1
+        
+        # estimate mean and deviation of the errors' vector
+        mean_valid, std_valid = (errors_valid.mean(), errors_valid.std())    
+                
+        # test
+        predictions = np.zeros(shape=y_test.shape)
+        y_test = y_test[:x_test.shape[0]]
+
+        # anomalies' statistics
+        errors_test = np.zeros(shape=len(y_test))
+        threshold = scistats.norm.pdf(mean_valid-2.*std_valid, mean_valid, std_valid)
+        anomalies = np.zeros(shape=len(y_test))
+        
+        iter_ = 0
+        
+        while iter_ < int(np.floor(x_test.shape[0] / batch_size)):
+    
+            batch_x = x_test[iter_*batch_size: (iter_+1)*batch_size, :, np.newaxis]
+            batch_y = y_test[iter_*batch_size: (iter_+1)*batch_size, np.newaxis]
+                
+            predictions[iter_] = sess.run(prediction, feed_dict={input_: batch_x,
+                                                                 target: batch_y})
+    
+            errors_test[iter_] = scistats.norm.pdf(predictions[iter_]-batch_y, mean_valid, std_valid)
+            anomalies = np.argwhere(errors_test < threshold)
+
+            iter_ +=  1
+            
+    # plot results
+    fig, ax1 = plt.subplots()
+
+    # plot data series
+    ax1.plot(y_test, 'b', label='index')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('TOPIX')
+
+    # plot predictions
+    ax1.plot(predictions[:y_test.shape[0]-batch_size+1], 'r', label='prediction')
+    ax1.set_ylabel('Change Point')
+    plt.legend(loc='best')
+
+    # highlights anomalies
+    for i in anomalies:
+        
+        if i <= len(y_test):
+            
+            plt.axvspan(i, i+1, color='yellow', alpha=0.5, lw=0)
+        
+    fig.tight_layout()
+    plt.show()
+               
---
+++ cnn-lstm/utils_topix.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  9 14:57:21 2018
+
+@author: Emanuele
+
+Data utilities for TOPIX prediction
+"""
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+
+"""
+ Measure randomness of a binary string.
+ Takes as input:
+     input_:numpy.array is the binary vector;
+     significance:float, the significance of the test.
+"""
+def random_test(input_, significance=5e-2):
+    
+    input_len = len(input_)
+    r = input_len
+    
+    ones = np.sum(input_)
+    zeros = input_len - ones
+    
+    r_hat = 2*(ones*zeros)/(ones+zeros) + 1 
+    s_r = ((2*ones*zeros)*(2*ones*zeros-ones-zeros))/((zeros+ones-1)*(ones+zeros)**2)
+    
+    z = (r - r_hat)/s_r
+    
+    # test is not random with this significance
+    if np.abs(z) > st.norm.ppf(1-significance/2):
+        
+        return False  
+    
+    else:
+        
+        return True
+    
+
+"""
+ Measure autocorrelation of a sequence with the lag test from Box and Jenkins, 1976.
+ Takes as input:
+     input_:numpy.array is the binary vector;
+     lag:int, the time-lag used to measure autocorrelation of the input sequence.
+     tolerance:float, the tolerance of the test.
+"""
+def autocorrelation_test(input_, lag, tolerance=1e-2):
+    
+    input_len = len(input_)
+    mean = input_.mean()
+    r_k_num = r_k_den = 0.
+    
+    for i in range(input_len-lag-1):
+        
+        r_k_num += (input_[i]-mean)*(input_[i+lag]-mean)
+        r_k_den += (input_[i]-mean)**2
+    
+    print(np.abs(r_k_num/(r_k_den+1e-10)), tolerance/2)
+    if np.abs(r_k_num/(r_k_den+1e-10)) <= tolerance/2:  # two tail test
+        
+        return True
+    
+    else:
+        
+        return False    
+    
+
+"""
+ Gaussian pdf estimator.
+ Takes as input:
+     x:numpy.array, the input vector;
+     mean:float, the mean of the gaussian distribution;
+     variance:float, the variance of the gaussian distribution.
+"""
+def gaussian_pdf(x, mean, variance):
+    
+    denom = (2*np.pi*variance)**.5
+    num = np.exp(-(float(x)-float(mean))**2/(2*variance))
+    
+    return num/denom
+
+
+"""
+ Turn a series into a matrix (i.e. repeated batches).
+"""
+def series_to_matrix(series, k_shape, striding=1):
+    
+    res = np.zeros(shape=(int((series.shape[0] - k_shape) / striding) + 1,
+                          k_shape)
+                   )
+    j = 0
+    for i in range(0, series.shape[0] - k_shape + 1, striding):
+        res[j] = series[i:i + k_shape]
+        j += 1
+
+    return res
+
+
+"""
+ 3 modes are possible and need do be specified in 'mode' variable:
+    'train': all data is reserved to train;
+    'train-test': split between train and test, according to non_train_percentage;
+    'validation': data is split among train, test and validation: their percentage is chosen according to the percantge
+                  of data that has not been included in train (1-non_train_percentage) and assigned to validation
+                  proportionally to val_rel_percentage.
+"""
+def generate_batches(filename, 
+                     window, 
+                     mode='train-test', 
+                     non_train_percentage=.7, 
+                     val_rel_percentage=.5,
+                     normalize=False):
+    
+    data = pd.read_csv(filename, delimiter=',', header=0)
+    data = (data.iloc[:, 0]).values
+    
+    if normalize is True:
+        
+        data = (data-np.min(data))/(np.max(data)-np.min(data))
+
+    if mode == 'train':
+
+        y = data[window:]
+        x = series_to_matrix(data, window, 1)[:-1]
+
+        return x, y
+
+    elif mode == 'train-test':
+
+        train_size = int((1 - non_train_percentage) * np.ceil(len(data)))
+        y_train = data[window:train_size]
+        x_train = series_to_matrix(data, window, 1)[:train_size - window]
+        y_test = data[train_size:]
+        x_test = series_to_matrix(data, window, 1)[train_size:]
+
+        return x_train, y_train, x_test, y_test
+
+    elif mode == 'validation':
+
+        # split between train and validation+test
+        train_size = int((1 - non_train_percentage) * np.ceil(len(data)))
+        y_train = data[window:train_size]
+        x_train = series_to_matrix(data, window, 1)[:train_size - window]
+
+        # split validation+test into validation and test
+        validation_size = int(val_rel_percentage * np.ceil(len(data) * non_train_percentage))
+        y_val = data[train_size:train_size + validation_size]
+        x_val = series_to_matrix(data, window, 1)[train_size - window:train_size + validation_size - window]
+        y_test = data[train_size + validation_size:]
+        x_test = series_to_matrix(data, window, 1)[train_size + validation_size - window:-window]
+
+        return x_train, y_train, x_val, y_val, x_test, y_test
+    
